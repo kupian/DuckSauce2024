f0e29005f81c6ab0957fa80d550c9ff674291a54
mcinfinn <mcinfinn@gmail.com>
2024-02-23 20:23:43 +0000

    added a test background

--- main.py
+++ main.py
@@ -16,20 +16,27 @@ x,y = pygame.display.get_window_size()
 x /=2
 y/=2
 
+
+
+bg=Player(screen,"art/test_bg.png",x,y)
 player = Player(screen, "art/static_duck.png", x, y)
 
+
+
 while running:
+    
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     
-    screen.fill("blue")
+
 
     box = DialogueBox(screen)
     box.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
-
+    bg.draw()
     player.draw()
-
+    
     keys = pygame.key.get_pressed()
     x,y = player.pos
     if keys[pygame.K_w]:
